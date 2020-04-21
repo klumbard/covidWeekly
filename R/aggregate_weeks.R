e6b0c4ee83d5c2b495f9a562ea203dcdbf8cd223
Keith Lumbard <KLumbard@gmail.com>
2020-04-21 16:29:57 -0400

    Added functionality for user-defined t0 (still testing)

--- R/aggregate_weeks.R
+++ R/aggregate_weeks.R
@@ -2,6 +2,7 @@
 #'
 #' @param dat A data frame of downloaded and cleaned state-level data from covidtracking.com, with absent negatives collapsed.
 #' @param agg_interval A positive whole number giving the desired length of aggregation intervals.
+#' @param t0 A date in the format "mm-dd-yyyy" which will serve as the "anchor" time from which we aggregate forwards
 #' @return
 #' A data frame grouped by state. Contains weekly aggregated counts of positive tests, negative tests, death, and hospitalizations.
 #' Data are grouped chronologically into "chunks" of length \code{agg_interval} starting from \code{2020-03-01}.
@@ -11,12 +12,13 @@
 #' dat2 <- clean_dat(dat)
 #' dat3 <- collapse_absent_negs(dat2)
 #' dat4 <- aggregate_weeks(dat3, agg_interval = 7)
-aggregate_weeks <- function(dat, agg_interval){
+aggregate_weeks <- function(dat, agg_interval, t0){
 
   dat_out <-
     dat %>%
+    dplyr::filter(date >= lubridate::ymd(t0)) %>%
     dplyr::mutate(aggGroup = floor((lubridate::ymd(date) -
-                                    lubridate::ymd("2020-03-01")) / agg_interval)) %>% # Group into intervals over which to aggregate
+                                    lubridate::ymd(t0)) / agg_interval)) %>% # Group into intervals over which to aggregate
     dplyr::mutate(epiWeek = lubridate::epiweek(date) + (lubridate::wday(date)/ 7)) %>%
     dplyr::group_by(state, aggGroup) %>%
     dplyr::summarise(epiWeek = dplyr::last(epiWeek),
--- R/get_t0.R
+++ R/get_t0.R
@@ -15,10 +15,10 @@
 #' dat3 <- collapse_absent_negs(dat2)
 #' dat4 <- aggregate_weeks(dat3)
 #' dat5 <- get_t0(dat, dat4)
-get_t0 <- function(dat_daily, dat_weekly, absent_negs = "remove"){
+get_t0 <- function(dat_weekly, absent_negs = "remove", agg_interval = NULL){
 
-  nstate <- length(unique(dat_daily$state))
-  t0_frame <- data.frame(state = unique(dat_daily$state),
+  nstate <- length(unique(dat_weekly$state))
+  t0_frame <- data.frame(state = unique(dat_weekly$state),
                          epiWeek = numeric(nstate),
                          endPt = as.Date(1:nstate, origin=Sys.Date()),
                          pos = NA,
@@ -28,21 +28,12 @@ get_t0 <- function(dat_daily, dat_weekly, absent_negs = "remove"){
                          death = NA,
                          t0 = 1)
 
-  if(absent_negs == "remove"){
-    for(i in 1:nrow(t0_frame)){
-      this_state <- t0_frame$state[i]
-      state_daily <-
-        dat_daily %>%
-        dplyr::filter(state == this_state) %>%
-        dplyr::arrange(date) %>%
-        dplyr::mutate(firstData = dplyr::first(date)) %>%
-        dplyr::filter(!is.na(negToday)) %>% # Remove all rows with NA negative test counts
-        dplyr::filter(!(dplyr::row_number() == 1 & date != firstData)) %>% # Remove the row after that, too
-        dplyr::slice(1)
 
-      this_date <- lubridate::ymd(state_daily$date[1] - 1)
-      t0_frame$epiWeek[i] <- lubridate::epiweek(this_date) + (lubridate::wday(this_date)/ 7)
-      t0_frame$endPt[i] <- lubridate::ymd(this_date)
+      for(i in 1:nrow(t0_frame)){
+      this_state <- dplyr::filter(dat_weekly, state == t0_frame$state[i])[1, ]
+      t0_frame$endPt[i] <- lubridate::ymd(this_state$endPt) - (agg_interval - 1)
+      t0_frame$epiWeek[i] <- lubridate::epiweek(t0_frame$endPt[i]) + (lubridate::wday(t0_frame$endPt[i])/ 7)
+
     }
 
     dat_out <-
@@ -51,29 +42,5 @@ get_t0 <- function(dat_daily, dat_weekly, absent_negs = "remove"){
       dplyr::arrange(state, epiWeek)
 
     dat_out
-
-  } else if(absent_negs == "collapse"){
-
-    for(i in 1:nrow(t0_frame)){
-      this_state <- t0_frame$state[i]
-      state_daily <-
-        dat_daily %>%
-        dplyr::filter(state == this_state) %>%
-        dplyr::arrange(date) %>%
-        dplyr::slice(1)
-
-      this_date <- state_daily$date - 1
-      t0_frame$epiWeek[i] <- lubridate::epiweek(this_date) + (lubridate::wday(this_date)/ 7)
-      t0_frame$endPt[i] <- lubridate::ymd(this_date)
-    }
-
-
-    dat_out <-
-      rbind.data.frame(t0_frame, as.data.frame(dat_weekly)) %>%
-      dplyr::group_by(state) %>%
-      dplyr::arrange(state, epiWeek)
-  }
-
-  dat_out
 
 }
